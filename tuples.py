c80b1ae72407180048e90e97e18839de75abcdc2
Emilio Araos <ea3dev@gmail.com>
2020-04-03 00:43:41 -0600

    unpacking

--- tuples.py
+++ tuples.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 lax_coordinates = (33.9425, -118.408045)
 city, year, pop, chg, area = ('Tokyo', 2003, 32450, 0.66, 8014)
 print(type(('Tokyo', 2003, 32450, 0.66, 8014)))
@@ -49,6 +51,29 @@ print('{:15} | {:^9} | {:^9}'.format('', 'lat.', 'long.'))
 fmt = '{:15} | {:9.4f} | {:9.4f}'
 for name, cc, pop, (latitude, longitude) in metro_areas:
     print(fmt.format(name, latitude, longitude))
+
+print('*' * 40)
+print('Namedtuples Example')
+
+City = namedtuple('City', 'name country population coordinates')
+tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139.691667))
+print(tokyo)
+print(tokyo.population)
+print(tokyo.coordinates)
+print(tokyo[1])
+
+print(City._fields)
+LatLong = namedtuple('LatLong', 'lat long')
+delhi_data = ('Delhi NCR', 'IN', 21.935, LatLong(28.613889, 77.208889))
+delhi = City._make(delhi_data)
+print(delhi._asdict())
+for key, value, in delhi._asdict().items():
+    print(key + ':', value)
+
+
+
+
+
 
 
 
